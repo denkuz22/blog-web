ca74e3f915e6ca52bd587d6fef9964e8cdd21712
denkuz22 <denis.kuznetsov@ryerson.ca>
2020-03-05 18:11:35 -0500

    Initial commit

---
+++ blog_web/myblog/models.py
@@ -0,0 +1,26 @@
+from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
+
+
+
+
+class Post(models.Model):
+  politics = 'POL'
+  tech = 'TECH'
+  env = 'ENVIRONMENT'
+  fin = 'FINANCE'
+  category_choices = (
+        (politics, 'Politics'),
+        (env, 'Environment'),
+        (tech, 'Tech'),
+        (fin, 'Finance'),
+    )
+  title = models.CharField(max_length=100)
+  content = models.TextField()
+  date_posted = models.DateTimeField(default=timezone.now)
+  author = models.ForeignKey(User,on_delete=models.CASCADE)
+  category = models.CharField(max_length=20,choices = category_choices,default = politics)
+  def __str__(self):
+      return self.title
+  
